ea41539b56c97b992b55495d3aa92c7c4ab0d44e
Maxxo0 <maximoamorebonapasta12@gmail.com>
2024-02-12 21:20:29 +0100

    Player Attack

--- MechMelodyURP/Assets/MechMelody_Root/Scripts/Bullet.cs
+++ MechMelodyURP/Assets/MechMelody_Root/Scripts/Bullet.cs
@@ -7,15 +7,27 @@ public class Bullet : MonoBehaviour
     [SerializeField] private float bulletSpeed = 5f;
     public bool isGrounded;
     public float bulletJumpForce = 5;
-  
+    public float lifeTime;
+    public float dir;
+    public Vector2 direction;
+    private float normalization;
+    private Vector2 normalizedOrientation;
+    Rigidbody2D bullet;
+    bool blue;
     
+
 
     // Start is called before the first frame update
     void Start()
     {
+
         Quaternion shootPointR = GameObject.Find("ShootPoint").transform.rotation;
         transform.rotation = shootPointR;
         isGrounded = true;
+        normalization = Mathf.Sqrt(Mathf.Pow(direction.x, 2) + Mathf.Pow(direction.y, 2));
+        normalizedOrientation = new Vector3(direction.x / normalization, direction.y / normalization);
+        bullet = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
@@ -26,33 +38,50 @@ public class Bullet : MonoBehaviour
         if (bulletJumpForce <= 0) { gameObject.SetActive(false); }
     }
 
+    private void FixedUpdate()
+    {
+        lifeTime -= Time.deltaTime;
+        if (blue) { bullet.velocity = new Vector3(dir * bulletSpeed * normalizedOrientation.x, bulletSpeed * normalizedOrientation.y, 0); }
+        {
+            
+        }
+    }
+
     public void BulletStatus()
     {
         switch (GameManager.Instance.actualMusicStatus)
         {
 
             case GameManager.MusicStatus.purple:
+                
                 bulletSpeed = 5f;
                 transform.Translate(Vector2.right * bulletSpeed * Time.deltaTime);
+                if (lifeTime <= 0) { gameObject.SetActive(false); }
                 break;
             case GameManager.MusicStatus.yellow:
-                Rigidbody2D yellowBullet = GetComponent<Rigidbody2D>();
+                
+                
                 bulletSpeed = 5f;
                 transform.Translate(Vector2.right * bulletSpeed * Time.deltaTime);
                 if (isGrounded)
                 {
                     isGrounded = false;
-                    yellowBullet.AddForce(Vector2.up * bulletJumpForce, ForceMode2D.Impulse);
+                    bullet.AddForce(Vector2.up * bulletJumpForce, ForceMode2D.Impulse);
                 }
-                 
+                if (lifeTime <= 0) { gameObject.SetActive(false); }
+
                 break;
             case GameManager.MusicStatus.orange:
+                
                 bulletSpeed = 50f;
                 transform.Translate(Vector2.right * bulletSpeed * Time.deltaTime);
+                if (lifeTime <= 0) { gameObject.SetActive(false); }
                 break;
             case GameManager.MusicStatus.blue:
-                bulletSpeed = 50f;
-                transform.Translate(Vector2.right * bulletSpeed * Time.deltaTime);
+                
+                bulletSpeed = 5f;
+                blue = true;
+                if (lifeTime <= 0) { gameObject.SetActive(false); }
                 break;
 
         }
@@ -64,6 +93,16 @@ public class Bullet : MonoBehaviour
         {
             bulletJumpForce--;
             isGrounded = true;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+
+            gameObject.SetActive(false);
         }
     }
 
--- MechMelodyURP/Assets/MechMelody_Root/Scripts/Shoot.cs
+++ MechMelodyURP/Assets/MechMelody_Root/Scripts/Shoot.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class Shoot : MonoBehaviour
 {
     [Header("Attack References")]
@@ -16,8 +17,10 @@ public class Shoot : MonoBehaviour
 
     [Header("Attack Stats")]
     public int condicion = 1;
-    public float fireRate =     0.5f;
+    public float fireRate = 0.5f;
     public bool canShoot;
+    
+    
 
 
 
@@ -36,6 +39,20 @@ public class Shoot : MonoBehaviour
 
     public void Attack(InputAction.CallbackContext context)
     {
+
+        float angle1 = 5f;
+        float angle2 = 2f;
+        float angle3 = -2f;
+        float angle4 = -5f;
+
+        Vector2 dir1 = CalculateVector(angle1);
+        Vector2 dir2 = CalculateVector(angle2);
+        Vector2 dir3 = CalculateVector(angle3);
+        Vector2 dir4 = CalculateVector(angle4);
+
+
+
+
         switch (GameManager.Instance.actualMusicStatus) 
         {
 
@@ -66,9 +83,22 @@ public class Shoot : MonoBehaviour
             case GameManager.MusicStatus.blue:
                 if (canShoot)
                 {
+                    GameObject b1 = Instantiate(bullet4, shootPoint.transform.position, shootPoint.transform.rotation);
+                    b1.GetComponent<Bullet>().dir = transform.localScale.x;
+                    b1.GetComponent<Bullet>().direction = dir1;
+                    GameObject b2 = Instantiate(bullet4, shootPoint.transform.position, shootPoint.transform.rotation);
+                    b2.GetComponent<Bullet>().dir = transform.localScale.x;
+                    b2.GetComponent<Bullet>().direction = dir2;
+                    GameObject b3 = Instantiate(bullet4, shootPoint.transform.position, shootPoint.transform.rotation);
+                    b3.GetComponent<Bullet>().dir = transform.localScale.x;
+                    b3.GetComponent<Bullet>().direction = dir3;
+                    GameObject b4 = Instantiate(bullet4, shootPoint.transform.position, shootPoint.transform.rotation);
+                    b4.GetComponent<Bullet>().dir = transform.localScale.x;
+                    b4.GetComponent<Bullet>().direction = dir4;
                     canShoot = false;
-                    Instantiate(bullet4, shootPoint.transform.position, shootPoint.transform.rotation);
                     Invoke(nameof(RestartShoot), fireRate);
+
+                    
                 }
                 break;
         }
@@ -79,6 +109,15 @@ public class Shoot : MonoBehaviour
         canShoot = true;
     }
 
-    
+    Vector2 CalculateVector(float angle)
+    {
+        var rad = Mathf.Sin((angle * Mathf.PI) / 180);
+        float Y1 = Mathf.Sin(rad);
+        float X1 = Mathf.Sqrt(1 - (Y1 * Y1));
+
+        Debug.Log(new Vector2(X1, Y1));
+
+        return new Vector2(X1, Y1);
+    }
 
 }
